14e2fc7dfb3e5dd685d1b098551a9051722ddc6f
JosephThomasPlappallil <jt287561@gmail.com>
2022-05-19 17:14:18 +0530

    added entity and resource

---
+++ src/main/java/org/strebentechnik/organization/OrganizationResource.java
@@ -0,0 +1,56 @@
+package org.strebentechnik.organization;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@Path("/organization")
+@Tag(name="Organizations")
+public class OrganizationResource {
+
+     // Create Employee
+     @POST
+     @Consumes(MediaType.APPLICATION_JSON)
+     @Produces(MediaType.APPLICATION_JSON)
+     @Transactional
+     public Response createOrganization(OrganizationEntity organizationEntity) {
+         organizationEntity.persist();
+         return Response.status(Status.CREATED).build();
+     }
+ 
+     // Get all employees
+     @GET
+     @Produces(MediaType.APPLICATION_JSON)
+     public Response listAllOrganiations() {
+         return Response.ok(OrganizationEntity.listAll()).build();
+     }
+ 
+     // Get employee by ID
+     @Path("/{id}")
+     @GET
+     @Produces(MediaType.APPLICATION_JSON)
+     public Response getOrganizationById(@PathParam("id") Long id) {
+         OrganizationEntity organizationEntity = OrganizationEntity.findById(id);
+         return Response.ok(organizationEntity).build();
+     }
+ 
+     // Delete employee by ID
+     @Path("/{id}")
+     @DELETE
+     @Produces(MediaType.APPLICATION_JSON)
+     @Transactional
+     public Response deleteOrganization(@PathParam("id") Long id) {
+         OrganizationEntity.deleteById(id);
+         return Response.noContent().build();
+     }
+    
+}
