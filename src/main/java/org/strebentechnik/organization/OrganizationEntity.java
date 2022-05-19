26c9a2d04768fa374999b5a8a8ca3ba4736136ce
JosephThomasPlappallil <jt287561@gmail.com>
2022-05-19 13:32:14 +0530

    first commit

---
+++ src/main/java/org/strebentechnik/organization/OrganizationEntity.java
@@ -0,0 +1,8 @@
+package org.strebentechnik.organization;
+import javax.persistence.Entity;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity(name="organization")
+public class OrganizationEntity extends PanacheEntity {
+    
+}
